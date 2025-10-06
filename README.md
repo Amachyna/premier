2b4dc2cac5c83376c65fff04587072f3ef6a4a41
Amachyna <lychal0411@gmail.com>
2025-10-06 09:57:37 +0200

    ajouter hello

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # premier
 Premier projet github, exercice nombre premier
 algorithme : euclide
+salut tout le monde
---
+++ premier.py
@@ -0,0 +1,14 @@
+n = int(input("Entrer un nombe: "))
+premier = True
+
+for diviseur in range (2,n):
+    reste = n % diviseur 
+    if reste == 0:
+        print("n, n'est pas premier car divisible par", diviseur)
+        premier = False
+
+if premier:
+    print(n, "est premier")
+
+
+print("hello")
